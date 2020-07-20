180bc39cd0b5d39519da5ffb8c9f8fc517a88e00
KiwiCode-s <38774014+KiwiCode-s@users.noreply.github.com>
2020-07-20 00:27:18 -0700

    finished

--- part06-Part06_10.TodoList/src/main/java/Main.java
+++ part06-Part06_10.TodoList/src/main/java/Main.java
@@ -5,12 +5,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Here you can try out the combined functionality of your classes
-        TodoList list = new TodoList();
-        Scanner scanner = new Scanner(System.in);
+//          Scanner scanner = new Scanner(System.in);
+//        TodoList list = new TodoList();
+//
+//        userInterface textUI = new userInterface(list, scanner);
+//        textUI.start();
+//        TodoList list = new TodoList();
+//        list.add("read the course material");
+//        list.add("watch the latest fool us");
+//        list.add("take it easy");
+//        list.print();
+//        list.remove(2);
+//        list.print();
+//        list.add("buy raisins");
+//        list.print();
+//        list.remove(1);
+//        list.remove(1);
+//        list.print();
 
-        UserInterface ui = new UserInterface(list, scanner);
-        ui.start();
+TodoList list = new TodoList();
+Scanner scanner = new Scanner(System.in);
+
+UserInterface ui = new UserInterface(list, scanner);
+ui.start();
 
     }
 }
