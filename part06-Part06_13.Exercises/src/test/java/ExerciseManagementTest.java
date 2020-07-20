3953a60906387e336a423fadf30148b4f49d4ce6
KiwiCode-s <38774014+KiwiCode-s@users.noreply.github.com>
2020-07-20 15:48:12 -0700

    Note one test fails
    Test fails because of add method most likly. I'll try to fix later at some point.

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,50 @@
-
-
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ExerciseManagementTest {
 
+    private ExerciseManagement management;
+
+    @Before
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+
+        assertEquals(0, management.exerciseList().size());
+    }
+
+    @Test
+    public void addingExerciseGrowsListByOne() {
+
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+
+    @Test
+    public void addedExcerciseIsInList() {
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+
+    }
+
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {    
+        management.add("New exercise");    
+        management.markAsCompleted("New exercise");    
+        assertTrue(management.isCompleted("New exercise"));
+        //this test fails because of the add method not correctly addeding im assumeing the beleow test doesnn't work as intened as well.
+        // ill try to fix it and understand it sometime later. I spent too long loocking into it right now
+    }
+    
+    
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {    
+        management.add("New exercise");    
+        management.markAsCompleted("New exercise");    
+        assertFalse(management.isCompleted("Some exercise"));
+    }
 }
